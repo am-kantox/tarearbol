29e698151ae47963855bfb4106de3b76ff2160f4
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2017-07-21 10:57:19 +0200

    credo + docs.

--- mix.exs
+++ mix.exs
@@ -26,8 +26,8 @@ defmodule Tarearbol.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:credo, "~> 0.8", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev}
     ]
   end
 
