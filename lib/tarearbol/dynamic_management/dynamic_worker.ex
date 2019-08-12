27e2bf7e62e8c5269d841ae5963f785619500876
Saverio Trioni <saverio.trioni@kantox.com>
2019-08-12 12:07:06 +0200

    Clearer worker implementation based on behaviours

--- lib/tarearbol/dynamic_management/dynamic_worker.ex
+++ lib/tarearbol/dynamic_management/dynamic_worker.ex
@@ -2,44 +2,43 @@ defmodule Tarearbol.DynamicWorker do
   @moduledoc false
   use GenServer
 
-  def start_link(id: id, manager: manager, runner: runner),
-    do: GenServer.start_link(__MODULE__, id: id, manager: manager, runner: runner)
+  def start_link(opts),
+    do:
+      GenServer.start_link(
+        __MODULE__,
+        opts |> Map.new() |> Map.put_new(:timeout, 1_000) |> Map.put_new(:lull, 1.1)
+      )
 
   @impl GenServer
   def init(opts) do
-    schedule_work()
+    schedule_work(opts.timeout)
     {:ok, opts}
   end
 
   @impl GenServer
-  def handle_info(:work, [id: id, manager: manager, runner: runner] = state) do
-    runner
-    |> case do
-      {m, f, a} -> apply(m, f, [id | a])
-      {m, f} -> apply(m, f, [id])
-      m when is_atom(m) -> apply(m, :runner, [id])
-      fun when is_function(fun, 1) -> fun.(id)
-    end
-    |> case do
+  def handle_info(:work, state) do
+    case state.manager.process(state.id, state.arg) do
       :halt ->
-        Tarearbol.InternalWorker.del(manager.internal_worker_module(), id)
+        Tarearbol.InternalWorker.del(manager.internal_worker_module(), state.id)
         {:noreply, state}
 
       result ->
         updated =
-          id
-          |> manager.state_module().get(%{})
+          state.id
+          |> manager.state_module().get()
           |> Map.put(:value, result)
 
-        manager.state_module().put(id, updated)
+        manager.state_module().put(state.id, updated)
 
-        schedule_work()
-        {:noreply, state}
+        schedule_work(state.timeout)
+        {:noreply, state, round(state.timeout * state.lull)}
     end
   end
+  
+  @impl GenServer
+  def handle_info(:timeout, state), do: state.manager.handle_timeout(state)
 
-  defp schedule_work do
-    # In a seconds
-    Process.send_after(self(), :work, 1_000)
+  defp schedule_work(timeout) do
+    Process.send_after(self(), :work, timeout)
   end
 end
