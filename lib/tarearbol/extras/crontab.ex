fc04ba0c155a8d2abfcc1844f4122463e6ef86df
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2019-08-30 06:27:14 +0200

    :bomb: in the middle of parsing

--- lib/tarearbol/extras/crontab.ex
+++ lib/tarearbol/extras/crontab.ex
@@ -1,54 +1,95 @@
 defmodule Tarearbol.Crontab do
   @moduledoc false
 
-  # minute hour day/month month day/week
+  @type t :: %__MODULE__{}
+  defstruct [:minute, :hour, :day, :month, :day_of_week]
+
+  @spec parse(input :: binary()) :: Tarearbol.Crontab.t()
+  @doc """
+  Parses the cron string into human-readable representation.
+
+  Format: ["minute hour day/month month day/week"](https://crontab.guru/).
+
+  _Examples:_
+
+      iex> Tarearbol.Crontab.parse("*/5 * * * *")
+      %Tarearbol.Crontab{hour: "*", day: "*", day_of_week: "*", minute: "*/5", month: "*"}
+
+  """
+
+  def parse("@yearly"), do: parse("0 0 1 1 *")
+
+  def parse("@monthly"), do: parse("0 0 1 * *")
+
+  def parse("@weekly"), do: parse("0 0 * * 1")
+
+  def parse("@daily"), do: parse("0 0 * * *")
+
+  def parse("@hourly"), do: parse("0 * * * *")
+
+  def parse("@reboot"), do: raise("Not supported")
+
+  def parse("@annually"), do: raise("Not supported")
+
+  def parse(input) when is_binary(input),
+    do: do_parse(input, {[:hour, :day, :month, :day_of_week], :minute, "", %{}})
+
+  #############################################################################
+
+  @spec do_parse(input :: binary(), {list[atom()], atom(), binary(), map()}) ::
+          Tarearbol.Crontab.t()
+  defp do_parse("", {[], frac, acc, result}),
+    do: struct(Tarearbol.Crontab, Map.put(result, frac, acc))
+
+  defp do_parse(" " <> rest, {fracs, frac, acc, result}) do
+    result = Map.put(result, frac, acc)
+    [frac | fracs] = fracs
+    do_parse(rest, {fracs, frac, "", result})
+  end
+
+  defp do_parse(<<c::binary-size(1), rest::binary>>, {fracs, frac, acc, result}),
+    do: do_parse(rest, {fracs, frac, acc <> c, result})
+
+  #############################################################################
+
   # *	any value
   # ,	value list separator
   # -	range of values
   # /	step values
-
-  @spec next_run(input :: binary()) :: DateTime.t()
-  @doc """
-  Calculates the `DateTime` instance based on cron string.
-
-  _Examples:_
-
-      iex> Tarearbol.Crontab.next_run("*/5 * * * *")
-      %{dm: "*", dw: "*", hour: "*", m: "*", min: "*/5"}
-
-  """
-
-  def next_run("@yearly"),
-    do: Tarearbol.Calendar.beginning_of(DateTime.utc_now(), 1, :year)
-
-  def next_run("@monthly"),
-    do: Tarearbol.Calendar.beginning_of(DateTime.utc_now(), 1, :month)
-
-  def next_run("@weekly"),
-    do: Tarearbol.Calendar.beginning_of(DateTime.utc_now(), 1, :week)
-
-  def next_run("@daily"),
-    do: Tarearbol.Calendar.beginning_of(DateTime.utc_now(), 1, :day)
-
-  def next_run("@hourly"),
-    do: Tarearbol.Calendar.beginning_of(DateTime.utc_now(), 1, :hour)
-
-  def next_run("@reboot"), do: raise("Not supported")
-
-  def next_run("@annually"), do: raise("Not supported")
-
-  def next_run(input) when is_binary(input),
-    do: do_next_run(input, {[:hour, :dm, :m, :dw], :min, "", %{}})
-
-  defp do_next_run("", {[], frac, acc, result}),
-    do: Map.put(result, frac, acc)
-
-  defp do_next_run(" " <> rest, {fracs, frac, acc, result}) do
-    result = Map.put(result, frac, acc)
-    [frac | fracs] = fracs
-    do_next_run(rest, {fracs, frac, "", result})
+  @spec cron_string_to_list(
+          dt :: DateTime.t(),
+          {:minute | :hour | :day | :month | :day_of_week, binary()}
+        ) :: Tarearbol.Crontab.t()
+  defp cron_string_to_list(dt \\ nil, {:minute, input}) do
   end
 
-  defp do_next_run(<<c::binary-size(1), rest::binary>>, {fracs, frac, acc, result}),
-    do: do_next_run(rest, {fracs, frac, acc <> c, result})
+  @spec parts(input :: binary()) :: [binary()]
+  defp parts(input) do
+    input
+    |> String.split(",")
+    |> Enum.flat_map(fn e ->
+      case String.split(e, "/") do
+        ["*", t] when byte_size(t) in [1, 2] ->
+          case Integer.parse(t) do
+            {int, ""} -> Stream.iterate(0, &(&1 + int))
+            _ -> :error
+          end
+
+        [s, t] when byte_size(s) in [1, 2] and byte_size(t) in [1, 2] ->
+          case {Integer.parse(s), Integer.parse(t)} do
+            {{from, ""}, {int, ""}} -> Stream.iterate(from, &(&1 + int))
+            _ -> :error
+          end
+
+        [t] when byte_size(t) in [1, 2] ->
+          case Integer.parse(t) do
+            {int, ""} -> Stream.iterate(0, &(&1 + int))
+            _ -> :error
+          end
+
+        _ ->
+          :error
+      end
+    end)
+  end
 end
