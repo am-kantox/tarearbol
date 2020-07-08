dc9fa4aedc64c56282b267173495535f767562f4
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2020-07-08 09:22:33 +0200

    Prepared for cleanup in Job/Errand

--- test/scheduler_test.exs
+++ test/scheduler_test.exs
@@ -13,7 +13,9 @@ defmodule Tarearbol.Scheduler.Test do
   end
 
   test "Job.create/3 + Scheduler.put/3" do
-    defmodule PingPong do
+    defmodule PingPong0 do
+      @moduledoc false
+
       @pid :erlang.term_to_binary(self())
       def run() do
         send(:erlang.binary_to_term(@pid), "pong")
@@ -21,13 +23,15 @@ defmodule Tarearbol.Scheduler.Test do
       end
     end
 
-    job = Tarearbol.Scheduler.Job.create(PutTestJob, &PingPong.run/0, "* * * * *")
-    Tarearbol.Scheduler.put("PutTestJob", %{payload: %{job: job}, timeout: 50})
-    assert_receive "pong", 200
+    job = Tarearbol.Scheduler.Job.create(PutTestJob, &PingPong0.run/0, "* * * * *")
+    Tarearbol.Scheduler.put("PutTestJob", %{payload: %{job: job}, timeout: 100})
+    assert_receive "pong", 500
   end
 
   test "push/3 → crontab" do
     defmodule PingPong1 do
+      @moduledoc false
+
       @pid :erlang.term_to_binary(self())
       def run() do
         send(:erlang.binary_to_term(@pid), "pong")
@@ -41,6 +45,8 @@ defmodule Tarearbol.Scheduler.Test do
 
   test "push/3 → DateTime" do
     defmodule PingPong2 do
+      @moduledoc false
+
       @pid :erlang.term_to_binary(self())
       def run() do
         send(:erlang.binary_to_term(@pid), "pong")
@@ -51,9 +57,9 @@ defmodule Tarearbol.Scheduler.Test do
     Tarearbol.Scheduler.push(
       PushTestJob2,
       &PingPong2.run/0,
-      DateTime.add(DateTime.utc_now(), 50, :millisecond)
+      DateTime.add(DateTime.utc_now(), 100, :millisecond)
     )
 
-    assert_receive "pong", 200
+    assert_receive "pong", 500
   end
 end
