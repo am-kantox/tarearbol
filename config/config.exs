27b19c71316a1e6a0afb0ea15553b8b4c0234264
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2020-10-26 16:54:54 +0100

    Cloister config: updated to the latest cloister options

--- config/config.exs
+++ config/config.exs
@@ -2,4 +2,8 @@ import Config
 
 config :tarearbol, persistent: false
 
+config :cloister,
+  sentry: ~w|tarearbol_1@127.0.0.1 inexisting@127.0.0.1|a,
+  consensus: 1
+
 if File.exists?("config/#{Mix.env()}.exs"), do: import_config("#{Mix.env()}.exs")
--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,3 @@
 import Config
 
-config :cloister,
-  sentry: ~w|tarearbol_1@127.0.0.1 inexisting@127.0.0.1|a,
-  consensus: 1
-
 config :telemetria, applications: [tarearbol: true]
