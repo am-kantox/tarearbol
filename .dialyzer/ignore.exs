b72415ac0709c460500906e45c07260d56396446
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2021-03-22 04:00:15 +0100

    Satisfy dialyzer

--- .dialyzer/ignore.exs
+++ .dialyzer/ignore.exs
@@ -1,4 +1,5 @@
 [
+  {"lib/tarearbol/jobs/job.ex", :call},
   {"lib/tarearbol/jobs/job.ex", :pattern_match},
   {"lib/tarearbol/jobs/errand.ex", :call},
   {"lib/tarearbol/extras/crontab.ex", :pattern_match},
