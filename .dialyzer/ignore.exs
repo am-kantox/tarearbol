3651fbe25cd934ef1a03dc1a3402674230339790
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2024-06-05 11:02:47 +0200

    Make dialyzer happy

--- .dialyzer/ignore.exs
+++ .dialyzer/ignore.exs
@@ -2,6 +2,5 @@
   {"lib/tarearbol/jobs/job.ex", :pattern_match},
   {"lib/tarearbol/jobs/job.ex", :call, 35},
   {"lib/tarearbol/jobs/errand.ex", :call},
-  {"lib/tarearbol/extras/crontab.ex", :pattern_match},
   {"lib/tarearbol/telemetria.ex", :pattern_match}
 ]
